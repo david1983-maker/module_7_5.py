de892417f93564aa5cd6d17c32005f13bcf4ebf5
david1983-maker <davidkilidzan94127@gmail.com>
2024-12-18 17:49:41 +0300

    Update module_7_5.py

--- module_7_5/module_7_5.py
+++ module_7_5/module_7_5.py
@@ -5,11 +5,11 @@ print('Текущая директория', os.getcwd())
 
 for root,dirs,files in os.walk('.'):
     for file in files:
-        filepath = os.path.join('.',file)
-        filetime = os.path.getatime(file)
+        filepath = os.path.join(root,file)
+        filetime = os.path.getatime(filepath)
         formatted_time = time.strftime("%d.%m.%H:%M", time.localtime(filetime))
-        filesize = os.path.getsize(file)
-        parent_dir = os.path.dirname(os.path.abspath(__file__))
+        filesize = os.path.getsize(filepath)
+        parent_dir = os.path.dirname(os.path.abspath(filepath))
         print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт,'
               f' Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
 
